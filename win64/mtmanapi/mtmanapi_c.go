8d8f6768dd150213e95f15e375ef7a997ffbe47c
cyrildou <cyrildou@live.cn>
2025-09-19 03:37:45 +0800

    support x64-23

--- win64/mtmanapi/mtmanapi_c.go
+++ win64/mtmanapi/mtmanapi_c.go
@@ -1,4 +1,5 @@
 package mtmanapi
 
-// #cgo windows,amd64 LDFLAGS: -lws2_32 -static
+// #cgo windows,amd64 LDFLAGS: -lws2_32 -lstdc++ -static
+// #cgo windows,amd64 CXXFLAGS: -target x86_64-pc-windows-msvc
 import "C"
